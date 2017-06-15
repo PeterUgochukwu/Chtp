ac8327dc018d466d5e75c7247aa3cc8726c02f96
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2017-06-15 07:06:23 -0700

    Create 2.30.java

---
+++ 2.30.java
@@ -0,0 +1,15 @@
+//java how to program 5th Edition Chapter 2 Exercise 2.30 Solution
+
+//import java packages
+import javax.swing.JOptionPane;
+
+public class checkerBoard2{
+  public static void main (String args[])
+  {
+    JOptionPane.showMessageDialog(null, "* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *");
+                              
+    
+    System.exit(0);
+    
+  }
+}
