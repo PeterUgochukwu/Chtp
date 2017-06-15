2f559b60afd7a1ab8d4890a626a214e947e2810e
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2017-06-15 06:28:36 -0700

    Create 2.29.java

---
+++ 2.29.java
@@ -0,0 +1,18 @@
+//java how to program 5th Edition Chapter 2 Exercise 2.29 Solution
+
+public class checkerBoard{
+  public static void main (String args[])
+  {
+    System.out.print("* * * * * * * *\n");
+    System.out.print(" * * * * * * * *\n");
+    System.out.print("* * * * * * * *\n");
+    System.out.print(" * * * * * * * *\n");
+    System.out.print("* * * * * * * *\n");
+    System.out.print(" * * * * * * * *\n");
+    System.out.print("* * * * * * * *\n");
+    System.out.println(" * * * * * * * *");
+    
+    System.exit(0);
+    
+  }
+}
