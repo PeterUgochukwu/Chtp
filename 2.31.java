8d5d6ba9dab28db5dfc7316a53861cbcb533ed9f
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2017-06-15 08:36:32 -0700

    Create 2.31.java

---
+++ 2.31.java
@@ -0,0 +1,24 @@
+//java how to program 5th Edition Chapter 2 Exercise 2.31 Solution
+
+public class unicode{
+  public static void main (String args[])
+  {
+    System.out.print("The Character " + 'A' + " has the value " + (int) 'A' + "\n");
+    System.out.print("The Character " + 'B' + " has the value " + (int) 'B' + "\n");
+    System.out.print("The Character " + 'C' + " has the value " + (int) 'C' + "\n");
+    System.out.print("The Character " + 'a' + " has the value " + (int) 'a' + "\n");
+    System.out.print("The Character " + 'b' + " has the value " + (int) 'b' + "\n");
+    System.out.print("The Character " + 'c' + " has the value " + (int) 'c' + "\n");
+    System.out.print("The Digit " + '0' + " has the value " + (int) '0' + "\n");
+    System.out.print("The Digit " + '1' + " has the value " + (int) '1' + "\n");
+    System.out.print("The Digit " + '2' + " has the value " + (int) '2' + "\n");
+    System.out.print("The Symbol " + '$' + " has the value " + (int) '$' + "\n");
+    System.out.print("The Symbol " + '*' + " has the value " + (int) '*' + "\n");
+    System.out.print("The Symbol " + '+' + " has the value " + (int) '+' + "\n");
+    System.out.print("The Symbol " + '/' + " has the value " + (int) '/' + "\n");
+    System.out.println("The Bank Character " + ' ' + " has the value " + (int) ' ' + "\n");
+    
+    System.exit(0);
+    
+  }
+}
