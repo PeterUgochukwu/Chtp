a9f8e67484e66293495bf6cdf811af179bfd08ba
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2017-07-06 07:14:40 -0700

    Create 4.26.java

---
+++ 4.26.java
@@ -0,0 +1,18 @@
+//Deitel Java how to program 5th Edition; Chapter 4 exercise 4.26 program solution
+//printing asteriks 
+
+public class Asteriks{
+  public static void main (String args[]){
+    
+    System.out.print("* * * * * * * *\n");
+    System.out.print(" * * * * * * * *\n ");
+    System.out.print("* * * * * * * *\n ");
+    System.out.print(" * * * * * * * *\n ");
+    System.out.print("* * * * * * * *\n ");
+    System.out.print(" * * * * * * * *\n ");
+    System.out.print("* * * * * * * *\n ");
+    System.out.println(" * * * * * * * * ");
+    
+    System.exit (0);
+  }
+}
