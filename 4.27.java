cefe9ce371ce54eeb2a803575cfc5ab6b6944782
Peter Ugochukwu Uche <pjgraphixs@gmail.com>
2017-07-06 07:24:39 -0700

    Create 4.27.java

---
+++ 4.27.java
@@ -0,0 +1,14 @@
+//Deitel Java how to program 5th Edition; Chapter 4 exercise 4.27 program solution
+
+public class MultipleOfTwo{
+  public static void main (String args[]){
+    
+    int i =2;
+    int number = 1;
+    
+    while (number ==1){
+      System.out.println(" "+ i);
+      i +=2;
+    }
+  }
+}
